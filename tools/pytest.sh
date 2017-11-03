329bd9aaec87bb2ef0f4cca3ab0253fea8fb523d
Andy Fingerhut <jafinger@cisco.com>
2017-11-03 16:56:29 -0700

    Add tools directory with some small utility programs

---
+++ tools/pytest.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Tiny convenience bash script that allows one to run:
+
+# sudo tools/pytest.sh
+
+# instead of having a shell always running as root.
+
+DEBUG="-d"
+#DEBUG=""
+
+set -x
+/bin/rm -f test.pcap
+set +x
+source my-venv/bin/activate
+set -x
+pytest
