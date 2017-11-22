e86e1909a6a23e271fced78d3e7c7976f5259dca
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-11-21 17:18:56 -0800

    Fix things broken by last commit
    os.path.isfile() returns False for whatever kind of special file that
    /tmp/bmv2-0-notifications.ipc is.  os.path.exists() returns True if it
    exists.

--- tools/run.sh
+++ tools/run.sh
@@ -160,5 +160,7 @@ set -x
 #p4pktgen ${OPTS} compiled_p4_programs/simple_ecmp.json
 #p4pktgen ${OPTS} compiled_p4_programs/simple_ecmp_no_verify.json
 
-p4pktgen ${OPTS} compiled_p4_programs/checksum-ipv4-with-options.json
+#p4pktgen ${OPTS} compiled_p4_programs/checksum-ipv4-with-options.json
 #p4pktgen ${OPTS} compiled_p4_programs/parse-ipv4-with-opts-no-lookahead.json
+
+p4pktgen ${OPTS} compiled_p4_programs/table-key-mask.json
