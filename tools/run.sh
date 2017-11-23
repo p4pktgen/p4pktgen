9a07d29f5f699489834b6601f274b9763232a074
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-11-22 21:34:16 -0800

    Eliminate many comments about old bugs long found & fixed in run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -14,8 +14,10 @@
 
 #OPTS=""
 #OPTS="-d"
+#OPTS="-d --disable-packet-length-errors"
+OPTS="--disable-packet-length-errors"
 #OPTS="-d --dump-test-case"
-OPTS="-d --dump-test-case --disable-packet-length-errors"
+#OPTS="-d --dump-test-case --disable-packet-length-errors"
 #OPTS="-d --allow-uninitialized-reads"
 #OPTS="-d --allow-uninitialized-reads --allow-unimplemented-primitives"
 #OPTS="--allow-uninitialized-reads --allow-unimplemented-primitives"
@@ -27,78 +29,40 @@ set +x
 source my-venv/bin/activate
 set -x
 
-# Infinite loop.  Issue open for this:
-# https://bitbucket.org/p4pktgen/p4pktgen/issues/13/having-same-action-name-on-multiple-tables
 #p4pktgen ${OPTS} compiled_p4_programs/demo1.p4_16.json
+# This one exhibited the bug with missing action ids, now fixed
+p4pktgen ${OPTS} compiled_p4_programs/demo1-no-uninit-reads.p4_16.json
+#p4pktgen ${OPTS} compiled_p4_programs/demo1-action-names-uniquified.p4_16.json
+#p4pktgen ${OPTS} compiled_p4_programs/demo1b.json
 
 #p4pktgen ${OPTS} compiled_p4_programs/demo1_rm_header.json
 #p4pktgen ${OPTS} compiled_p4_programs/add-remove-header.json
 
 #p4pktgen ${OPTS} compiled_p4_programs/config-table.json
 
-# 2017-Sep-20
-# Crashes with "Exception: Primitive op drop not supported"
 #p4pktgen ${OPTS} compiled_p4_programs/demo1-action-names-uniquified.p4_16.json
 
-p4pktgen ${OPTS} compiled_p4_programs/demo1b.json
 #p4pktgen ${OPTS} compiled_p4_programs/read-ingress-port.json
 
-# Similar crash as for demo1-action-names-uniquified.p4_16.json
 #p4pktgen ${OPTS} compiled_p4_programs/demo2.p4_16.json
 
 #p4pktgen ${OPTS} compiled_p4_programs/demo3-parser-verify.json
 
-# No crash
 #p4pktgen ${OPTS} compiled_p4_programs/demo8.json
-
-# Crashes because ipv4.$valid$ KeyError.  Issue open for this:
-# https://bitbucket.org/p4pktgen/p4pktgen/issues/14/define-valid-fields-in-all-headers
 #p4pktgen ${OPTS} compiled_p4_programs/demo8-compiled-2017-sep-01-p4c.json
 
 #p4pktgen ${OPTS} compiled_p4_programs/demo8b.json
 
-# Opened issue for latest reason that this program crashes p4pktgen
 #p4pktgen ${OPTS} compiled_p4_programs/demo9.json
-
 #p4pktgen ${OPTS} compiled_p4_programs/demo9b.json
 
-# Crashes for similar reason as demo9.json
-#p4pktgen ${OPTS} compiled_p4_programs/demo9-alt.json
-
-# Crashes because ? operator not implemented yet
-#p4pktgen ${OPTS} compiled_p4_programs/demo8-tiny2.json
-
-# Runs without crashing, but miscompares on simple_switch results.  I
-# think the solver may be 'believing' it has found a packet that can
-# exercise an impossible path, where the first ingress 'if' condition
-# is true, but the second is false, but that cannot happen.
-#
-# Opened issue for this:
-# https://bitbucket.org/p4pktgen/p4pktgen/issues/16/sample-p4-programs-where-perhaps-the
 #p4pktgen ${OPTS} compiled_p4_programs/demo10.json
 #p4pktgen ${OPTS} compiled_p4_programs/demo10b.json
 
-# Created bitbucket issue for a crash with demo11.json:
-# https://bitbucket.org/p4pktgen/p4pktgen/issues/17/bad-assert-failure-when-generating
 #p4pktgen ${OPTS} compiled_p4_programs/demo11.json
 
-# Looking at the expected and actual paths in the output of this run,
-# it appears that everything is working as it should.  Yay!
 #p4pktgen ${OPTS} compiled_p4_programs/demo12.json
 
-# This one has a problem in simple_switch with the state that checks
-# frag offset?  Bug in simple_switch?  Antonin Bas claims it is a bug
-# in p4c-bm2-ss.
-#
-# demo14-hand-edited.json was created by copying demo14.json and
-# replacing a string "0xff" with "0x00ff" as suggested by Antonin Bas,
-# and it does indeed seem to fix the issue.  Hence the p4c issue
-# below, if fixed in p4c, should lead to correct behavior in
-# simple_switch.
-#
-# Issues created here:
-# https://github.com/p4lang/p4c/issues/914
-# https://github.com/p4lang/behavioral-model/issues/441
 #p4pktgen ${OPTS} compiled_p4_programs/demo13.json
 #p4pktgen ${OPTS} compiled_p4_programs/demo14.json
 #p4pktgen ${OPTS} compiled_p4_programs/demo14-hand-edited.json
@@ -113,14 +77,6 @@ p4pktgen ${OPTS} compiled_p4_programs/demo1b.json
 # demo16.p4 exercises many comparison and boolean operators.  I have
 # checked results by hand and they look correct.
 #p4pktgen ${OPTS} compiled_p4_programs/demo16.json
-
-# Crashes for same reason as demo8-compiled-2017-sep-01-p4c.json
-# above, but only with change to src/main/translator.py to change
-# $valid$ field to type bit<1> instead of bool.
-#p4pktgen ${OPTS} compiled_p4_programs/demo8-tiny4.json
-
-#p4pktgen ${OPTS} compiled_p4_programs/demo8-tiny5.json
-#p4pktgen ${OPTS} compiled_p4_programs/demo8-alternate.json
 
 #p4pktgen ${OPTS} compiled_p4_programs/switch-p416.json
 
@@ -144,6 +100,8 @@ p4pktgen ${OPTS} compiled_p4_programs/demo1b.json
 # Looking better!
 #p4pktgen ${OPTS} compiled_p4_programs/chksum.json
 
+#p4pktgen ${OPTS} compiled_p4_programs/chksum-incremental-wrong-rfc1624-eqn2-issue983-workaround.json
+
 #p4pktgen ${OPTS} compiled_p4_programs/chksum2.json
 #p4pktgen ${OPTS} compiled_p4_programs/chksum3.json
 #p4pktgen ${OPTS} compiled_p4_programs/chksum4.json
@@ -163,4 +121,4 @@ p4pktgen ${OPTS} compiled_p4_programs/demo1b.json
 #p4pktgen ${OPTS} compiled_p4_programs/checksum-ipv4-with-options.json
 #p4pktgen ${OPTS} compiled_p4_programs/parse-ipv4-with-opts-no-lookahead.json
 
-p4pktgen ${OPTS} compiled_p4_programs/table-key-mask.json
+#p4pktgen ${OPTS} compiled_p4_programs/table-key-mask.json
